5594b6098dd47c37e90bc34b1cb2422bc1fdb6f5
Ondra Hajny <hajnyon@gmail.com>
2018-09-10 22:08:54 +0200

    added jumping WIP

--- src/Duck.js
+++ src/Duck.js
@@ -1,10 +1,10 @@
 /**
  * Implements duck class.
- * 
+ *
  * @class Duck
  */
 class Duck {
-  
+
   /**
    * Creates an instance of Duck.
    * @param {Integer} y Y coord of duck.
@@ -14,40 +14,43 @@ class Duck {
     this.x = width + 20; // X coord (always behind canvas)
     this.y = y; // Y coord
     this.r = 30; // radius
-    this.type = random([true, false]); // one of two type of ducks
+    this.gravity = 1;
+    this.type = random(images);
   }
 
   /**
    * Draws duck to canvas.
-   * 
+   *
    * @memberof Duck
    */
   show() {
 
-    // no stroke 
     noStroke();
-    // add transparent color
     fill(255, 0);
-    // draw ellipse
-    ellipse(this.x, this.y, this.r*2, this.r*2);
-    // set one of the images and move it so its about in center
-    image(this.type ? duck1 : duck2, this.x - 25, this.y - 30);
-    
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    image(this.type.image, this.x - 25, this.y - 30, 50, 50);
+
   }
 
   /**
    * Moves the duck.
-   * 
+   *
    * @memberof Duck
    */
   move() {
-    // move it to the left TODO: add speed, gravity simulation etc.
+    // TODO: add speed, gravity simulation etc.
     this.x = this.x - 2;
+    this.y += this.gravity;
+    this.gravity += 0.15;
+    if (this.gravity > 5) {
+      this.gravity = 0;
+      this.y -= 85;
+    }
   }
 
   /**
    * Checks if duck intersects with suplied coordinates.
-   * 
+   *
    * @param {Integer} x X coord.
    * @param {Integer} y Y coord
    * @returns True if intersects false otherwise.
@@ -56,8 +59,8 @@ class Duck {
   intersect(x, y) {
 
     // dummy check TODO: add checking by shape
-    if(x < this.x + this.r && y < this.y + this.r &&
-       x > this.x - this.r && y > this.y - this.r) {
+    if (x < this.x + this.r && y < this.y + this.r &&
+      x > this.x - this.r && y > this.y - this.r) {
       return true;
     } else {
       return false;
